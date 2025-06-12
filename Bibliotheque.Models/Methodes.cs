898320091793c5ae88d37317030b7b786a138981
ManonDamoiseau <m.damoiseau05@hotmail.com>
2025-06-12 16:54:35 +0200

    Créa Menu + classe methode + methode newBook, showAll (à resoudre) , quitter

---
+++ Bibliotheque.Models/Methodes.cs
@@ -0,0 +1,30 @@
+﻿
+using static System.Reflection.Metadata.BlobBuilder;
+using System;
+
+namespace Bibliotheque.Models
+{
+    public class Methodes
+    {
+        //Afficher le menu
+            public static void Menu()
+            {
+                Console.WriteLine("Bonjour, que souhaitez vous faire?");
+                Console.WriteLine("1 = Encoder un nouveau livre");
+                Console.WriteLine("2 = Voir tous les livres de la bibliothèque");
+                Console.WriteLine("0 = Supprimer la bibliothèque");
+            }
+            
+            public static void newBook()
+            {
+                Console.WriteLine("====== NOUVEAU LIVRE ======\n");
+                Console.WriteLine("Quel est le titre du livre?");
+                
+            }
+        public static void showAllBooks()
+        {
+            Console.WriteLine("====== LIVRE(S) DE LA BIBLIOTHEQUE ======");
+            
+        }
+    }
+}
--- Bibliothèque/Program.cs
+++ Bibliothèque/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using Bibliotheque.Models;
 
 namespace Bibliothèque
 {
@@ -7,21 +9,45 @@ namespace Bibliothèque
     {
         static void Main(string[] args)
         {
+
+            bool over = false;
             int num = 1;
-            Dictionary<int, string> books = new Dictionary<int, string>();
 
-            Console.WriteLine("Bonjour, que souhaitez vous faire?");
-            Console.WriteLine("1 = Encoder un nouveau livre");
-            int.TryParse(Console.ReadLine(), out int instructUser);
-            if (instructUser == 1)
+            while (over == false)
             {
-                //méthode newBook
+                Dictionary<int, string> books = new Dictionary<int, string>();
+
+                //Méthode menu
+                Methodes.Menu();
+                int.TryParse(Console.ReadLine(), out int instructUser);
+                switch (instructUser)
+                {
+                    case 1:
+                        //méthode newBook
+                        Console.Clear();
+                        Methodes.newBook();
+                        books.Add(num, Console.ReadLine());
+                        num += 1;
+                        break;
+
+                    case 2:
+                        //méthode showAllBooks
+                        //Méthode montrer un livre
+                        Console.Clear();
+                        Methodes.showAllBooks();
+                        foreach (KeyValuePair<int, string> kvp in books)
+                        {
+                            Console.WriteLine($"{0} {1}, kvp.Key, kvp.Value");
+                        }
+                        break;
+
+                    case 0:
+                        over = true;
+                        break;
+
+                }
             }
-
-
-            //Méthode newBook
-            books.Add(num, Console.ReadLine());
-                num + 1;
         }
     }
 }
+
