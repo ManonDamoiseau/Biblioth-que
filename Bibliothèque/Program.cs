5d23e5abad598f3506ed14ed7921b7444b9ddd70
ManonDamoiseau <m.damoiseau05@hotmail.com>
2025-06-12 15:00:25 +0200

    Ajoutez des fichiers projet.

---
+++ Bibliothèque/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bibliothèque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            Dictionary<int, string> books = new Dictionary<int, string>();
+
+            Console.WriteLine("Bonjour, que souhaitez vous faire?");
+            Console.WriteLine("1 = Encoder un nouveau livre");
+            int.TryParse(Console.ReadLine(), out int instructUser);
+            if (instructUser == 1)
+            {
+                //méthode newBook
+            }
+
+
+            //Méthode newBook
+            books.Add(num, Console.ReadLine());
+                num + 1;
+        }
+    }
+}
